3567cddc67e4641b0f8f45e013cc853e5b304b57
prince kashyap <vasukashyap08@gmail.com>
2023-05-15 22:53:31 +0530

    commit

--- main.js
+++ main.js
@@ -14,3 +14,26 @@ window.addEventListener("scroll", () => {
     const header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY > 100);
 });
+
+//  owlcarousal 
+$(".carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 3,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false
+        },
+    }
+});
